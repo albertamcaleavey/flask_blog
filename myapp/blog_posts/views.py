dc1e4c792d410a8463b1a0ba34acfd7bc23237ee
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-04-07 14:25:39 -0400

    add show controller

--- myapp/blog_posts/views.py
+++ myapp/blog_posts/views.py
@@ -6,7 +6,7 @@ from myapp.blog_posts.forms import BlogPostForm
 
 blog_posts = Blueprint('blog_posts', __name__)
 
-# to create a blog post
+# create controller
 @blogposts.route('/create', methods=['GET', 'POST'])
 # protects this controller/ route
 @login_required
@@ -24,4 +24,11 @@ def create_post():
   # else, return form to create a post ?
   return render_template('create_post.html', form=form)
 
-  
+# show controller
+# variable for dynamic routing we will pass
+@blog_posts.route('/<int:blog_post_id>')
+def blog_post(blog_post_id):
+  # query for blog post
+  # get the post, or throw 404
+  blog_post = BlogPost.query.get_or_404(blog_post_id)
+  return render_template('blog_post.html', title=blog_post.title, date=blog_post.date, post=blog_post)
